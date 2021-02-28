d7d95cd0b5da33fae162e3dc4aedd14fd8c00131
Martin M <martin7mondaca@gmail.com>
2021-02-27 23:00:01 -0700

    commented out routes for testing

--- server.js
+++ server.js
@@ -11,37 +11,37 @@ app.use(express.json());
 
 
 app.get("/", function (req, res) {
-    res.json(path.join(__dirname, "public/index.html"))
-})
+    res.json(path.join(__dirname, "public/index.html"));
+});
 
-app.get("/notes", function (req, res) {
-    // fs.readFile("./db/db.json", (err, data) => {
-    //     if (err) {
-    //         console.error(err)
-    //         return
-    //     }
-    //     res.json(data)
-    // })
-    res.sendFile(path.join(__dirname, "public/notes.html"))
-})
+// app.get("/notes", function (req, res) {
+//     // fs.readFile("./db/db.json", (err, data) => {
+//     //     if (err) {
+//     //         console.error(err)
+//     //         return
+//     //     }
+//     //     res.json(data)
+//     // })
+//     res.sendFile(path.join(__dirname, "public/notes.html"))
+// })
 
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"))
-})
+// app.get("*", function (req, res) {
+//     res.sendFile(path.join(__dirname, "public/index.html"))
+// })
 
-app.get("/api/notes", function (req, res) {
-    //res.sendFile(path.join(__dirname, "db.json"))
-    return res.json("db/db.json")
-})
+// app.get("/api/notes", function (req, res) {
+//     //res.sendFile(path.join(__dirname, "db.json"))
+//     return res.json("db/db.json")
+// })
 
-app.post("api/notes", function (req, res) {
-    fs.appendFile("./db/db.json")
-    // var newNote = req.body;
-    // res.json(newNote)
-})
+// app.post("api/notes", function (req, res) {
+//     fs.appendFile("./db/db.json")
+//     // var newNote = req.body;
+//     // res.json(newNote)
+// })
 
-app.delete("")
+// app.delete("")
 
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-})
+// app.listen(PORT, function () {
+//     console.log("App listening on PORT " + PORT);
+// })
