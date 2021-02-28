c5c359808014e176ba3f17df400fe9bf20a76f60
Martin M <martin7mondaca@gmail.com>
2021-02-28 02:48:56 -0700

    split html and api routes into separate js files

--- server.js
+++ server.js
@@ -4,45 +4,17 @@ var app = express();
 var fs = require("fs")
 var PORT = process.env.PORT || 3000;
 
-var notes = require("./db/db.json")
+var notes = require("./db/db.json");
+const { json } = require("express");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-app.get("/notes", function (req, res) {
-    // fs.readFile("./db/db.json", (err, data) => {
-    //     if (err) {
-    //         console.error(err)
-    //         return
-    //     }
-    //     res.json(data)
-    // })
-    res.sendFile(path.join(__dirname, "public/notes.html"))
-})
-
-// app.get("*", function (req, res) {
-//     res.sendFile(path.join(__dirname, "public/index.html"))
-// })
-
-// app.get("/api/notes", function (req, res) {
-//     //res.sendFile(path.join(__dirname, "db.json"))
-//     return res.json("db/db.json")
-// })
-
-// app.post("api/notes", function (req, res) {
-//     fs.appendFile("./db/db.json")
-//     // var newNote = req.body;
-//     // res.json(newNote)
-// })
-
-// app.delete("")
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-})
+});
